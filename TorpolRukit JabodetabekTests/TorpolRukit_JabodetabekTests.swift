314d4d873ef39a982233f57bac7e228ab95147d8
Miky Setiawan <mikysetiawan@gmail.com>
2020-04-19 17:27:37 +0700

    Initial Commit

---
+++ TorpolRukit JabodetabekTests/TorpolRukit_JabodetabekTests.swift
@@ -0,0 +1,34 @@
+//
+//  TorpolRukit_JabodetabekTests.swift
+//  TorpolRukit JabodetabekTests
+//
+//  Created by Miky Setiawan on 19/04/20.
+//  Copyright © 2020 Miky Technology. All rights reserved.
+//
+
+import XCTest
+@testable import TorpolRukit_Jabodetabek
+
+class TorpolRukit_JabodetabekTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
